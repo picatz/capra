cc7c7e0881a7fe7ff1c5a03b9d1f6c9ce0033c6c
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-04-15 23:21:27 -0400

    Update engine.rb

--- lib/capra/engine.rb
+++ lib/capra/engine.rb
@@ -1,45 +1,100 @@
 module Capra
-    class Engine 
-      attr_accessor :interface
-      attr_accessor :rules
-  
-      def initialize(file: nil, &block)
-        @interface = Interfacez.default
-        @rules     = {}
-        if file
-          instance_eval File.read(file)
-        else
-          instance_eval &block
+  class Engine 
+    attr_accessor :interface
+    attr_accessor :rules
+
+    def initialize(file: nil, &block)
+      default_interface
+      @rules = {}
+      if file
+        instance_eval File.read(file)
+      else
+        instance_eval &block
+      end
+      start!
+    end
+
+    def interface(iface)
+      @interface = iface
+    end
+
+    def default_interface
+      @interface = Interfacez.default
+    end
+
+    def pcap(file)
+      @pcap = file
+    end
+
+    def save_to(file)
+      @save_to = file
+    end
+
+    def debug!
+      binding.pry
+    end
+
+    def rule(type, description: nil, reference: nil, &block)
+      if @rules[type]
+        @rules[type] << block
+      else
+        @rules[type] = [block]
+      end
+    end
+
+    def alert(mesg)
+      puts mesg
+    end
+
+    def email(recpt)
+      puts "Sending email!"
+    end
+
+    def save(packet)
+      @save_to = "capra-save-"+Time.now.utc.to_s.split(" ").join("-")+".pcapng" if @save_to.nil?
+
+      pf = PacketGen::PcapNG::File.new
+      pf.array_to_file [packet]
+      pf.to_f(@save_to, append: true)
+    end
+
+    def start!
+      if @pcap
+        read_pcap_file(@pcap) do |packet|
+          @rules.each do |header, blocks|
+            if header == 'ANY' || packet.is?(header)
+              blocks.each do |block|
+                block.call(packet)
+              end
+            end
+          end
         end
-        start!
-      end
-  
-      def rule(type, description: nil, reference: nil, &block)
-        if @rules[type]
-          @rules[type] << block
-        else
-          @rules[type] = [block]
-        end
-      end
-  
-      def alert(mesg)
-        puts mesg
-      end
-  
-      def email(recpt)
-        puts "Sending email!"
-      end
-  
-      def start!
+      else
         PacketGen.capture(iface: @interface) do |packet|
           @rules.each do |header, blocks|
-            next unless packet.is? header
-            blocks.each do |block|
-              block.call(packet)
+            if header == 'ANY' || packet.is?(header)
+              blocks.each do |block|
+                block.call(packet)
+              end
             end
           end
         end
       end
     end
+
+    private
+
+    def read_pcap_file(filename)
+      PcapNG::File.new.read_packets(filename) do |packet|
+        yield packet
+      end
+    rescue StandardError => e
+      PCAPRUB::Pcap.open_offline(filename).each_packet do |packet|
+        next unless (packet = PacketGen.parse(packet.to_s))
+
+        yield packet
+      end
+    end
   end
-  
+end
+
