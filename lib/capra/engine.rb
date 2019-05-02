88d93a106c450d9f5b4594d895029390cc73f821
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-05-02 10:57:22 -0400

    Update engine.rb

--- lib/capra/engine.rb
+++ lib/capra/engine.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Capra
-  class Engine 
+  class Engine
     attr_accessor :interface
     attr_accessor :rules
 
@@ -30,10 +32,6 @@ module Capra
       @save_to = file
     end
 
-    def debug!
-      binding.pry
-    end
-
     def rule(type, description: nil, reference: nil, &block)
       if @rules[type]
         @rules[type] << block
@@ -46,12 +44,12 @@ module Capra
       puts mesg
     end
 
-    def email(recpt)
-      puts "Sending email!"
+    def email(_recpt)
+      puts 'Sending email!'
     end
 
     def save(packet)
-      @save_to = "capra-save-"+Time.now.utc.to_s.split(" ").join("-")+".pcapng" if @save_to.nil?
+      @save_to = 'capra-save-' + Time.now.utc.to_s.split(' ').join('-') + '.pcapng' if @save_to.nil?
 
       pf = PacketGen::PcapNG::File.new
       pf.array_to_file [packet]
@@ -62,20 +60,20 @@ module Capra
       if @pcap
         read_pcap_file(@pcap) do |packet|
           @rules.each do |header, blocks|
-            if header == 'ANY' || packet.is?(header)
-              blocks.each do |block|
-                block.call(packet)
-              end
+            next unless header == 'ANY' || packet.is?(header)
+
+            blocks.each do |block|
+              block.call(packet)
             end
           end
         end
       else
         PacketGen.capture(iface: @interface) do |packet|
           @rules.each do |header, blocks|
-            if header == 'ANY' || packet.is?(header)
-              blocks.each do |block|
-                block.call(packet)
-              end
+            next unless header == 'ANY' || packet.is?(header)
+
+            blocks.each do |block|
+              block.call(packet)
             end
           end
         end
@@ -88,7 +86,7 @@ module Capra
       PcapNG::File.new.read_packets(filename) do |packet|
         yield packet
       end
-    rescue StandardError => e
+    rescue StandardError
       PCAPRUB::Pcap.open_offline(filename).each_packet do |packet|
         next unless (packet = PacketGen.parse(packet.to_s))
 
@@ -97,4 +95,3 @@ module Capra
     end
   end
 end
-
