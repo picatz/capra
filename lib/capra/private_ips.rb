13b481fe1c7c572fa41b09a076fb6006556af581
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-05-02 10:57:26 -0400

    Update private_ips.rb

--- lib/capra/private_ips.rb
+++ lib/capra/private_ips.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module Capra
-    PRIVATE_IPS = [
-      IPAddr.new('10.0.0.0/8'),
-      IPAddr.new('172.16.0.0/12'),
-      IPAddr.new('192.168.0.0/16'),
-    ].freeze
+  PRIVATE_IPS = [
+    IPAddr.new('10.0.0.0/8'),
+    IPAddr.new('172.16.0.0/12'),
+    IPAddr.new('192.168.0.0/16')
+  ].freeze
 end
